cb9a96aa3e3e9a031d98c7f342c478109fb34c47
Richard Annand <richard+git@ohheyrj.co.uk>
2022-09-23 21:17:51 +0100

    fix: reverted to kubectl_manifest

--- custom_resource_definition.tf
+++ custom_resource_definition.tf
@@ -2,7 +2,7 @@ data "kubectl_path_documents" "external_dns" {
   pattern = "${path.module}/external-dns-crd.yaml"
 }
 
-resource "kubernetes_manifest" "external_dns" {
-  for_each = toset(data.kubectl_path_documents.cert_manager.documents)
-  manifest = yamldecode(each.value)
+resource "kubectl_manifest" "external_dns" {
+  for_each  = toset(data.kubectl_path_documents.external_dns.documents)
+  yaml_body = each.value
 }
